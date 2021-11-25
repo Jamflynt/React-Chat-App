f36186e4e969bc3df33ceb63aefe48e65f9f88f9
Jamie Flynt <jamflynt@gmail.com>
2021-11-24 20:10:08 -0500

    Rewrote index.js to fix React.DOM not defined issue. Added Chat ID, username, secret from chatengine.io.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="8daafad3-64f5-4565-b295-ec4c73ccc365"
+            userName="Jamflynt"
+            userSecret="123123"
+
+        
+        />
+    )
+}
+
+export default App;
