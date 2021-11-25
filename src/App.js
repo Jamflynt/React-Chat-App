eafb4a6b56c7dbe623f2a7033eddecce73a6baca
Jamie Flynt <jamflynt@gmail.com>
2021-11-24 21:06:35 -0500

    Added components folder. Created ChatFeed.jsx, MessageForm.jsx, MyMessages.jsx, TheirMessages.jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
 const App = () => {
@@ -9,10 +11,10 @@ const App = () => {
             projectID="8daafad3-64f5-4565-b295-ec4c73ccc365"
             userName="Jamflynt"
             userSecret="123123"
-
+            renderChatFeed ={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         
         />
-    )
+    );
 }
 
 export default App;
