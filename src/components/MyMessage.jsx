79bfaa60e0634846df612686231ea7af12e3d77f
Jamie Flynt <jamflynt@gmail.com>
2021-11-25 22:07:15 -0500

    Wrote MyMessage component.

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,7 +1,18 @@
 const MyMessage = () => {
+    // Here is the question mark again, checks to see if its there before it loads it
+    if(message?.attachements?.length > 0){
+        return (
+            <img
+                scr={message.attachements[0].file}
+                alt="message-attachment"
+                className="message-image"
+                style={{float: 'right'}}
+            />
+        )
+    }
     return (
-        <div>
-            MyMessage
+        <div className="message" style={{float: 'right', marginRight='18px', color: 'white', backgroundColor: '#3b2a50'}}>
+            {message.text}
         </div>
     )
 
