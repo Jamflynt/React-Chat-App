618fb26a2e0c8a89edd67ecf2077713c5a9d2664
Jamie Flynt <jamflynt@gmail.com>
2021-11-27 23:08:46 -0500

    Created inital login form. Added if statment to App.js for localstorage username.

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import axios from 'axios';
+
+const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const authObject = { 'Project-ID': "8daafad3-64f5-4565-b295-ec4c73ccc365", 'User-Name': username, 'User-Secret': password }
+
+        try{
+            // username / password => chatengine -> give messages
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+            // works out -> logged in 
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+
+            window.location.reload();
+
+        } catch(error){
+            // error -> try with new usename...
+
+        }
+
+
+    }
+
+    return(
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required/>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required/>
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Start Chatting</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    )
+}
+
+export default LoginForm;
